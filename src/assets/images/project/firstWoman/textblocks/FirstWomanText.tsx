3dbd0add8d050f0deb7cf3a5d29f47ba3a3848f5
andyagus <Redagus@gmail.com>
2024-01-23 10:24:10 -0500

    set up gitlfs for files

--- src/assets/images/project/firstWoman/textblocks/FirstWomanText.tsx
+++ src/assets/images/project/firstWoman/textblocks/FirstWomanText.tsx
@@ -1,6 +1,3 @@
-import React, { useEffect, useState } from 'react';
-
-
-
-export let text1 = "hello";   
-
+version https://git-lfs.github.com/spec/v1
+oid sha256:ba130bea66aeca7746237342bf352228ae001eed08c1df143668ba9f4b37823f
+size 87
