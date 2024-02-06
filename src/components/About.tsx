0dba80c656477ab45138ba8fa425f25dfd7e7956
andyagus <Redagus@gmail.com>
2024-02-06 17:45:41 -0500

    updates to about page

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,18 +1,29 @@
 // import { FaGithub, FaLinkedin, FaInstagram, FaSpotify } from 'react-icons/fa';
 
+import { Link } from "react-router-dom";
 import ImageGallery from "./ImageGallery.tsx";
+import ProjectHeaderText from "./Project/projectComponents/ProjectHeaderText.tsx";
+import ProjectSubcaptionText from "./Project/projectComponents/ProjectSubcaptionText.tsx";
+import ProjectParagraphText from "./Project/projectComponents/ProjectParagraphText.tsx";
 
 export default function About() {
   return (
-    <div className=" mt-24  mx-auto p-6 md:max-w-6xl m-12">
-      <div className="grid grid-cols-1 lg:grid-cols-3 ">
-        <div className="lg:col-span-1 lg:order-2">
-          <ImageGallery />
-        </div>
-        <div className="lg:col-span-2 lg:mr-52">
-          <div className="mt-10 mb-4 text-3xl font-semibold">About Me</div>
-          <div className="text-base text-gray-500">
-            I am deeply interested in Augmented Reality and its potential to
+    <div className="mt-24  mx-auto p-6">
+      <div className="max-w-xl ml-5 md:max-w-4xl lg:mx-auto">
+        {/*go back arrow*/}
+        <Link
+          to={"/"}
+          className="flex items-center group lg:ml-200 py-2"
+        ></Link>
+        <br />
+        <ProjectHeaderText title="About Me" />
+        <ProjectSubcaptionText text="Augmented Reality designer and prototyper dedicated to crafting technology that enhances our world" />
+        <br />
+        <ImageGallery />
+        <br />
+        <br />
+        <ProjectParagraphText
+          text="I am deeply interested in Augmented Reality and its potential to
             improve the world we live in. In 2020, I chose to pivot from
             entrepreneurship to concentrate fully on AR, embarking on a learning
             journey with Unity and embracing a role as a UX designer at Bully
@@ -31,34 +42,71 @@ export default function About() {
             prototypes. This pursuit, now my full-time endeavor, fuels my
             excitement for the field. My aim is to work with an exceptional team
             to create meaningful AR products that make a real difference in how
-            we interact with our environment.
-          </div>
-          <br />
-          <div className="text-base text-gray-500">
-            Email me at{" "}
-            <a
-              href="mailto:redagus@gmail.com"
-              className="text-red-600 hover:text-gray-500 underline hover:underline decoration-transparent hover:decoration-current transition-colors duration-300"
-            >
-              Redagus@gmail.com
-            </a>{" "}
-            {"   ||   "}
-            <a
-              href="mailto:redagus@gmail.com"
-              className="text-red-500 hover:text-gray-500 underline hover:underline decoration-transparent hover:decoration-current transition-colors duration-300"
-            >
-              View my resume
-            </a>
-            {"   ||   "}
-            <a
-              href="mailto:redagus@gmail.com"
-              className="text-red-400 hover:text-gray-500 underline hover:underline decoration-transparent hover:decoration-current transition-colors duration-300"
-            >
-              LinkedIn
-            </a>
-          </div>
+            we interact with our environment."
+        />
+        <br /> <br />
+        <div className="text-base text-gray-500">
+          Email me at{" "}
+          <a
+            href="mailto:redagus@gmail.com"
+            className="text-red-600 hover:text-gray-500 underline hover:underline decoration-transparent hover:decoration-current transition-colors duration-300"
+          >
+            Redagus@gmail.com
+          </a>{" "}
+          {"   ||   "}
+          <a
+            href="mailto:redagus@gmail.com"
+            className="text-red-500 hover:text-gray-500 underline hover:underline decoration-transparent hover:decoration-current transition-colors duration-300"
+          >
+            View my Resume (PDF)
+          </a>
+          {"   ||   "}
+          <a
+            href="mailto:redagus@gmail.com"
+            className="text-red-400 hover:text-gray-500 underline hover:underline decoration-transparent hover:decoration-current transition-colors duration-300"
+          >
+            Connect on LinkedIn
+          </a>
         </div>
       </div>
     </div>
+
+    // <div className=" mt-24  mx-auto p-6 md:max-w-6xl m-12">
+    //   <div className="grid grid-cols-1 lg:grid-cols-3 ">
+    //     <div className="lg:col-span-1 lg:order-2">
+    //       <ImageGallery />
+    //     </div>
+    //     <div className="lg:col-span-2 lg:mr-52">
+    //       <div className="mt-10 mb-4 text-3xl font-semibold">About Me</div>
+    //       <div className="text-base text-gray-500">
+
+    //       </div>
+    //       <br />
+    //       <div className="text-base text-gray-500">
+    //         Email me at{" "}
+    //         <a
+    //           href="mailto:redagus@gmail.com"
+    //           className="text-red-600 hover:text-gray-500 underline hover:underline decoration-transparent hover:decoration-current transition-colors duration-300"
+    //         >
+    //           Redagus@gmail.com
+    //         </a>{" "}
+    //         {"   ||   "}
+    //         <a
+    //           href="mailto:redagus@gmail.com"
+    //           className="text-red-500 hover:text-gray-500 underline hover:underline decoration-transparent hover:decoration-current transition-colors duration-300"
+    //         >
+    //           View my resume
+    //         </a>
+    //         {"   ||   "}
+    //         <a
+    //           href="mailto:redagus@gmail.com"
+    //           className="text-red-400 hover:text-gray-500 underline hover:underline decoration-transparent hover:decoration-current transition-colors duration-300"
+    //         >
+    //           LinkedIn
+    //         </a>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
   );
 }
--- src/components/ImageGallery.tsx
+++ src/components/ImageGallery.tsx
@@ -1,13 +1,17 @@
 import { Gallery, GalleryProps } from "react-grid-gallery";
-import image1 from "../assets/images/About/ProfilePic1.png";
-import image2 from "../assets/images/About/pic2.jpg";
+import image1 from "../assets/images/About/image1.jpg";
+import image2 from "../assets/images/About/image2.jpg";
+import image3 from "../assets/images/About/image3.jpg";
+import image4 from "../assets/images/About/image4.jpg";
+import image5 from "../assets/images/About/image5.png";
+
 import { useState } from "react";
 import { Image } from "react-grid-gallery";
 
 import Lightbox from "yet-another-react-lightbox";
 import "yet-another-react-lightbox/styles.css";
 
-// interface Image {
+// interface Image {`
 //   src: string;
 //   original?: string;
 //   thumbnail: string;
@@ -27,7 +31,7 @@ const images: CustomImage[] = [
     original: image1,
 
     width: 512,
-    height: 683,
+    height: 613,
     customOverlay: <div className=" bg-gray-900 opacity-15 h-full"> </div>,
   },
   {
@@ -39,16 +43,24 @@ const images: CustomImage[] = [
     customOverlay: <div className=" bg-gray-900 opacity-15 h-full"> </div>,
   },
   {
-    src: image2,
-    original: image2,
+    src: image3,
+    original: image3,
+
+    width: 200,
+    height: 600,
+    customOverlay: <div className=" bg-gray-900 opacity-15 h-full"> </div>,
+  },
+  {
+    src: image4,
+    original: image4,
 
     width: 500,
     height: 500,
     customOverlay: <div className=" bg-gray-900 opacity-15 h-full"> </div>,
   },
   {
-    src: image2,
-    original: image2,
+    src: image5,
+    original: image5,
 
     width: 500,
     height: 500,
