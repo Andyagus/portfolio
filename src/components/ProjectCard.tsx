e40075a1290c654616bc4b4bf52c12fbefd2bfd2
andyagus <Redagus@gmail.com>
2024-02-06 12:53:26 -0500

    update

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,10 +1,7 @@
-// import React, { Component } from 'react';
-import { useState } from "react";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 interface ProjectCardProps {
-  // Props here
   image: string;
   video: string;
   title: string;
@@ -12,47 +9,24 @@ interface ProjectCardProps {
 }
 
 export default function ProjectCard(props: ProjectCardProps) {
+  // State to manage hover effect
+  const [isHovered, setIsHovered] = useState(false);
+
+  // Destructure props for easy access
   const { image, video, title, description } = props;
 
-  const handleScroll = () => {
-    const scrollY = window.scrollY; // Don't get confused by what's scrolling - It's not the window
-    if (scrollY > 10 && scrollY < 200) {
-      if (title == "NASA: First Woman") {
-        setIsHovered(true);
-      }
-    } else if (scrollY > 240 && scrollY < 400) {
-      if (title == "ocV!be Smart City") {
-        setIsHovered(true);
-      }
-    } else if (scrollY > 300 && scrollY < 340) {
-      if (title == "BookShelf") {
-        setIsHovered(true);
-      }
-    } else if (scrollY > 400 && scrollY < 500) {
-      if (title == "Museum") {
-        setIsHovered(true);
-      }
-    } else {
-      setIsHovered(false);
-    }
-  };
-
+  // Determine if the device is mobile
+  const [isMobile, setIsMobile] = useState(false);
   useEffect(() => {
-    const isMobile = window.matchMedia("(max-width: 768px)").matches;
+    const mediaQuery = window.matchMedia("(max-width: 768px)");
+    setIsMobile(mediaQuery.matches);
 
-    if (isMobile) {
-      window.addEventListener("scroll", handleScroll);
-    }
-
-    return () => {
-      if (isMobile) {
-        window.removeEventListener("scroll", handleScroll);
-      }
-    };
+    const handler = () => setIsMobile(mediaQuery.matches);
+    mediaQuery.addListener(handler);
+    return () => mediaQuery.removeListener(handler);
   }, []);
 
-  const [isHovered, setIsHovered] = useState(false); // Set isHovered to false by default
-
+  // Format the title string for URL
   let formattedString = title
     .replace(/\s+/g, "-")
     .replace(/:/g, "")
@@ -63,13 +37,12 @@ export default function ProjectCard(props: ProjectCardProps) {
     <div>
       <Link to={`/projects/${formattedString}`}>
         <div
-          onMouseEnter={() => setIsHovered(true)} // Set isHovered to true on mouse enter
-          onMouseLeave={() => setIsHovered(false)} // Set isHovered to false on mouse leave
-          className="group cursor-pointer bg-white rounded-xl 
-            shadow-md overflow-hidden relative "
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+          className="group cursor-pointer bg-white rounded-xl shadow-md overflow-hidden relative"
         >
           <div>
-            {isHovered ? (
+            {isHovered || isMobile ? (
               <video
                 className="object-cover w-full h-96"
                 src={video}
@@ -88,15 +61,11 @@ export default function ProjectCard(props: ProjectCardProps) {
 
           <div className="bg">
             <div
-              className={`absolute bottom-0 mb-5 p-4 bg-white bg-opacity-90 rounded-xl 
-                shadow-lg w-full max-w-[260px] md:max-w-xs lg:max-w-sm mx-auto left-0 right-0 ${
-                  isHovered ? "opacity-90" : "opacity-0"
-                } duration-500 `}
+              className={`absolute bottom-0 mb-5 p-4 bg-white bg-opacity-90 rounded-xl shadow-lg w-full max-w-[260px] md:max-w-xs lg:max-w-sm mx-auto left-0 right-0 ${
+                isHovered || isMobile ? "opacity-90" : "opacity-0"
+              } duration-500 `}
             >
-              <span
-                className="text-base
-                  md:text-base lg:text-lg font-bold"
-              >
+              <span className="text-base md:text-base lg:text-lg font-bold">
                 {title}
               </span>
               <p className="text-xs md:text-sm lg:text-base text-gray-700 mt-2">
