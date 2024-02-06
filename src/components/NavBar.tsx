da0fe90e1317c178e7ac1f6fea3142cfbe4d08ab
andyagus <Redagus@gmail.com>
2024-02-06 16:30:26 -0500

    up;comign - adjusting gallery

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -44,8 +44,12 @@ const NavBar = () => {
         }
       >
         <div className="flex flex-col items-start p-10 mt-20">
-          <div className="py-6  text-3xl">Projects</div>
-          <div className="py-6 text-3xl">About</div>
+          <a className="py-6  text-3xl" href="/">
+            Projects
+          </a>
+          <a href="/about" className="py-6  text-3xl">
+            About
+          </a>
         </div>
       </div>
     </div>
