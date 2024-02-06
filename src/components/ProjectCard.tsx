7767a13288ecf0065e1cec1ba96cb1b3b3a04e72
andyagus <Redagus@gmail.com>
2024-02-06 13:08:44 -0500

    updates

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,7 +1,8 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 interface ProjectCardProps {
+  // Props here
   image: string;
   video: string;
   title: string;
@@ -9,24 +10,11 @@ interface ProjectCardProps {
 }
 
 export default function ProjectCard(props: ProjectCardProps) {
-  // State to manage hover effect
-  const [isHovered, setIsHovered] = useState(false);
-
-  // Destructure props for easy access
   const { image, video, title, description } = props;
+  const isMobile = window.matchMedia("(max-width: 768px)").matches;
 
-  // Determine if the device is mobile
-  const [isMobile, setIsMobile] = useState(false);
-  useEffect(() => {
-    const mediaQuery = window.matchMedia("(max-width: 768px)");
-    setIsMobile(mediaQuery.matches);
+  const [isHovered, setIsHovered] = useState(false); // Set isHovered to false by default
 
-    const handler = () => setIsMobile(mediaQuery.matches);
-    mediaQuery.addListener(handler);
-    return () => mediaQuery.removeListener(handler);
-  }, []);
-
-  // Format the title string for URL
   let formattedString = title
     .replace(/\s+/g, "-")
     .replace(/:/g, "")
@@ -37,12 +25,13 @@ export default function ProjectCard(props: ProjectCardProps) {
     <div>
       <Link to={`/projects/${formattedString}`}>
         <div
-          onMouseEnter={() => setIsHovered(true)}
-          onMouseLeave={() => setIsHovered(false)}
-          className="group cursor-pointer bg-white rounded-xl shadow-md overflow-hidden relative"
+          onMouseEnter={() => setIsHovered(true)} // Set isHovered to true on mouse enter
+          onMouseLeave={() => setIsHovered(false)} // Set isHovered to false on mouse leave
+          className="group cursor-pointer bg-white rounded-xl 
+            shadow-md overflow-hidden relative "
         >
           <div>
-            {isHovered || isMobile ? (
+            {isHovered ? (
               <video
                 className="object-cover w-full h-96"
                 src={video}
@@ -61,11 +50,15 @@ export default function ProjectCard(props: ProjectCardProps) {
 
           <div className="bg">
             <div
-              className={`absolute bottom-0 mb-5 p-4 bg-white bg-opacity-90 rounded-xl shadow-lg w-full max-w-[260px] md:max-w-xs lg:max-w-sm mx-auto left-0 right-0 ${
-                isHovered || isMobile ? "opacity-90" : "opacity-0"
-              } duration-500 `}
+              className={`absolute bottom-0 mb-5 p-4 bg-white bg-opacity-90 rounded-xl 
+                shadow-lg w-full max-w-[260px] md:max-w-xs lg:max-w-sm mx-auto left-0 right-0 ${
+                  isHovered || isMobile ? "opacity-90" : "opacity-0"
+                } duration-500 `}
             >
-              <span className="text-base md:text-base lg:text-lg font-bold">
+              <span
+                className="text-base
+                  md:text-base lg:text-lg font-bold"
+              >
                 {title}
               </span>
               <p className="text-xs md:text-sm lg:text-base text-gray-700 mt-2">
