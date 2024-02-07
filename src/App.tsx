88cfa8e577170e7b9eca613e010a7faa1a9b9860
andyagus <Redagus@gmail.com>
2024-02-07 14:52:50 -0500

    ga4

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Project from "./components/Project.tsx";
 import About from "./components/About.tsx";
 import Footer from "./components/Footer.tsx";
 import { Route, Routes } from "react-router-dom";
-import ReactGA from "react-ga";
+import ReactGA from "react-ga4";
 
 const measurementId = "G-79VS217Y6J";
 ReactGA.initialize(measurementId);
