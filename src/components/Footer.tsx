311a3cedf94100ca6949f8864badf6a14523ed5a
andyagus <Redagus@gmail.com>
2024-02-07 10:15:38 -0500

    commented out social link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,14 @@
-import { FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
-import ProjectIcon from "./Project/projectComponents/ProjectIcon";
-import ProjectIcons from "./Project/projectComponents/ProjectIcons";
-import { MdEmail } from "react-icons/md";
+// import { FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
+// import ProjectIcon from "./Project/projectComponents/ProjectIcon";
+// import ProjectIcons from "./Project/projectComponents/ProjectIcons";
+// import { MdEmail } from "react-icons/md";
 
 export default function Footer() {
   return (
     <>
       <div className="h-24 w-full border-t border-grey-500 flex">
         <div className="flex-1 flex items-center justify-start">
-          <div className="text-sm md:text-base md:ml-36 ml-9">
+          {/* <div className="text-sm md:text-base md:ml-36 ml-9">
             <ProjectIcons
               projectIcon1={
                 <ProjectIcon icon={MdEmail} link="mailto:redagus@gmail.com" />
@@ -32,7 +32,7 @@ export default function Footer() {
                 />
               }
             />
-          </div>
+          </div> */}
         </div>
         <div className="flex-1 flex items-center justify-end">
           <div className="text-sm md:text-base lg:mt-6 opacity-85 mr-9 md:mr-36">
